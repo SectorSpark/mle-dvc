c6b2e397426e8d82ee03bdbc4c97bea51c2258fe
SectorSpark <sectorspark@gmail.com>
2024-08-13 19:30:16 +0000

    Model change

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,11 +1,8 @@
-# scripts/fit.py
-
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
 import yaml
 import os
 import joblib
@@ -13,32 +10,26 @@ import joblib
 # обучение модели
 def fit_model():
     with open('params.yaml', 'r') as fd:
-        params = yaml.safe_load(fd)	
-    # Прочитайте файл с гиперпараметрами params.yaml
-
-	# загрузите результат предыдущего шага: inital_data.csv
+        params = yaml.safe_load(fd)
+        
+    # загрузите результат предыдущего шага: inital_data.csv
     data = pd.read_csv('data/initial_data.csv')
-	# реализуйте основную логику шага с использованием гиперпараметров
-    # обучение модели
+    
+    # реализуйте основную логику шага с использованием гиперпараметров
     cat_features = data.select_dtypes(include='object')
-    potential_binary_features = cat_features.nunique() == 2
-
-    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
-    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
     num_features = data.select_dtypes(['float'])
-
+    
     preprocessor = ColumnTransformer(
         [
-        ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
-        ('cat', CatBoostEncoder(return_df=False), other_cat_features.columns.tolist()),
-        ('num', StandardScaler(), num_features.columns.tolist())
+            ('cat', OneHotEncoder(drop='if_binary'), cat_features.columns.tolist()),
+            ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
-
-    model = CatBoostClassifier(auto_class_weights='Balanced')
-
+    
+    model = LogisticRegression(C=params['C'], penalty=params['penalty'], max_iter=1000)
+    
     pipeline = Pipeline(
         [
             ('preprocessor', preprocessor),
@@ -46,11 +37,11 @@ def fit_model():
         ]
     )
     pipeline.fit(data, data['target'])
-	# сохраните обученную модель в models/fitted_model.pkl
+    
+    # сохраните обученную модель в models/fitted_model.pkl
     os.makedirs('models', exist_ok=True)
     with open('models/fitted_model.pkl', 'wb') as fd:
-        joblib.dump(pipeline, fd) 
-    
+        joblib.dump(pipeline, fd)
 
 if __name__ == '__main__':
-	fit_model()
+    fit_model()
