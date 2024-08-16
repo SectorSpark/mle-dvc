d6de9b11f6696c39647beca22f60b1bf1b147770
SectorSpark <sectorspark@gmail.com>
2024-08-16 13:00:34 +0000

    change pipeline

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,5 +1,3 @@
-# scripts/evaluate.py
-
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold, cross_validate
 import joblib
@@ -7,16 +5,15 @@ import json
 import yaml
 import os
 
-# оценка качества модели
 def evaluate_model():
     with open('params.yaml', 'r') as fd:
         params = yaml.safe_load(fd)
-	# прочитайте файл с гиперпараметрами params.yaml
-    pipeline = joblib.load('models/fitted_model.pkl') #models/fitted_model.pkl
-	# загрузите результат прошлого шага: fitted_model.pkl
-    # Проверка качества на кросс-валидации
+    
+    pipeline = joblib.load('models/fitted_model.pkl')
+    
     data = pd.read_csv('data/initial_data.csv')
-    cv_strategy = StratifiedKFold(n_splits=5)
+    
+    cv_strategy = StratifiedKFold(params['n_splits'])
     cv_res = cross_validate(
         pipeline,
         data,
@@ -24,15 +21,14 @@ def evaluate_model():
         cv=cv_strategy,
         n_jobs=-1,
         scoring=['f1', 'roc_auc']
-        )
+    )
+    
     for key, value in cv_res.items():
         cv_res[key] = round(value.mean(), 3)
+    
     os.makedirs('cv_results', exist_ok=True)
-	# реализуйте основную логику шага с использованием прочтённых гиперпараметров
     with open('cv_results/cv_res.json', 'w') as json_file:
         json.dump(cv_res, json_file, indent=4)
-	# сохраните результата кросс-валидации в cv_res.json
-
 
 if __name__ == '__main__':
-	evaluate_model()
+    evaluate_model()
--- scripts/fit.py
+++ scripts/fit.py
@@ -1,35 +1,41 @@
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from category_encoders import CatBoostEncoder
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.linear_model import LogisticRegression
+from catboost import CatBoostClassifier
 import yaml
 import os
 import joblib
+from sklearn.linear_model import LogisticRegression
 
-# обучение модели
+
 def fit_model():
     with open('params.yaml', 'r') as fd:
         params = yaml.safe_load(fd)
-        
-    # загрузите результат предыдущего шага: inital_data.csv
+    
     data = pd.read_csv('data/initial_data.csv')
-    
-    # реализуйте основную логику шага с использованием гиперпараметров
+    # обучение модели
     cat_features = data.select_dtypes(include='object')
-    num_features = data.select_dtypes(['float64', 'int64'])
-    
+    potential_binary_features = cat_features.nunique() == 2
+
+    binary_cat_features = cat_features[potential_binary_features[potential_binary_features].index]
+    other_cat_features = cat_features[potential_binary_features[~potential_binary_features].index]
+    num_features = data.select_dtypes(['float'])
+
     preprocessor = ColumnTransformer(
         [
-            ('cat', OneHotEncoder(drop='if_binary'), cat_features.columns.tolist()),
-            ('num', StandardScaler(), num_features.columns.tolist())
+        ('binary', OneHotEncoder(drop='if_binary'), binary_cat_features.columns.tolist()),
+        ('cat', OneHotEncoder(), other_cat_features.columns.tolist()),
+        ('num', StandardScaler(), num_features.columns.tolist())
         ],
         remainder='drop',
         verbose_feature_names_out=False
     )
-    
+
     model = LogisticRegression(C=params['C'], penalty=params['penalty'], max_iter=1000)
-    
+
+
     pipeline = Pipeline(
         [
             ('preprocessor', preprocessor),
@@ -37,11 +43,10 @@ def fit_model():
         ]
     )
     pipeline.fit(data, data['target'])
-    
-    # сохраните обученную модель в models/fitted_model.pkl
+    feature_names = pipeline.named_steps['preprocessor'].get_feature_names_out()
+    print(feature_names)
     os.makedirs('models', exist_ok=True)
-    with open('models/fitted_model.pkl', 'wb') as fd:
-        joblib.dump(pipeline, fd)
+    joblib.dump(pipeline, 'models/fitted_model.pkl')
 
 if __name__ == '__main__':
     fit_model()
